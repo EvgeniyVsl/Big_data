d1c0f3fbe91d58e02c5df6e6dda326f823e3fadc
EvgeniyVsl <146645746+EvgeniyVsl@users.noreply.github.com>
2025-03-11 19:18:47 +1000

    Задача про Фибоначчи

---
+++ task02.py
@@ -0,0 +1,26 @@
+"""
+Given a cell with "it's a fib sequence" from slideshow,
+    please write function "check_fib", which accepts a Sequence of integers, and
+    returns if the given sequence is a Fibonacci sequence
+
+We guarantee, that the given sequence contain >= 0 integers inside.
+
+"""
+
+from collections.abc import Sequence
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    
+    if len(data) < 2:
+        return False
+
+    for i in range(2, len(data)):
+        if data[i] != data[i - 1] + data[i - 2]:
+            return False
+
+    return True
+
+print(check_fibonacci([0, 1, 1, 2, 3, 5, 8, 13]))  # True
+print(check_fibonacci([0, 1, 1, 2, 3, 5, 8, 14]))  # False
+print(check_fibonacci([0])) # False
+print(check_fibonacci([])) # False
